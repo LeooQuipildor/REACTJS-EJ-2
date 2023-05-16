c7c4c34883f2a8cf09c5fe0b728e7eea63ebad86
Leandro Quipildor <83482016+LeooQuipildor@users.noreply.github.com>
2023-05-16 04:09:06 -0300

    Agregamos la estructura y ademas un componente que muestra un mensaje mediante props en el index.html

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Titulo from './components/Titulo'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Titulo nuevoTitulo="Hello my friend!"></Titulo>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Titulo = ({nuevoTitulo}) => {
+    return (
+        <div>
+            <h1>{nuevoTitulo}</h1>
+        </div>
+    );
+};
+
+export default Titulo;
